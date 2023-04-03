7a968cfa0b066c2e13e24d968f056f1356cd5609
Apdo-25 <abdo0062@easv365.dk>
2023-04-03 17:08:43 +0200

    project setup

---
+++ Backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+//swagger
+// const swaggerUI = require("swagger-ui-express");
+// const yaml = require("yamljs");
+
+//swagger setup
+// const swaggerDocument = yaml.load("./swagger.yaml");
+// app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// import routes
+
+require("dotenv-flow").config();
+
+// parse request - content type JSON
+app.use(bodyParser.json());
+
+// mongoose.set('strictQuery', false);
+
+// mongoose.connect(
+
+//   process.env.DBHOST, {
+
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+//   }
+// ).catch(error => console.log("Error conncecting to MongoDB:" + error));
+
+// mongoose.connection.once("open", () => console.log("Connected succesfully to MongoDB"));
+
+//routes
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to the Car REST API homepage" });
+});
+
+// post, put, delete
+
+const PORT = process.env.PORT || 4000;
+
+//start up server on port 4000
+app.listen(PORT, function () {
+  console.log("Server is running on port: " + PORT);
+});
+
+module.exports = app;
